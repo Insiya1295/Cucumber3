6958b76fb33271d02a59e0413d43ca1d6c56a21c
Insiya1295 <54269294+Insiya1295@users.noreply.github.com>
2019-08-23 14:41:36 +0530

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -20,86 +20,158 @@ import io.cucumber.datatable.DataTable;
 
 
 public class Stepdefs {
+	static WebDriver driver=null;
+	@Given("User launch the site")
+public void user_launch_the_site() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_h2a.06.15\\Desktop\\SeleniumBroDrivers\\SeleniumBroDrivers\\chromedriver.exe\\");
+	driver =new ChromeDriver();
+	driver.get("http://10.232.237.143:443/TestMeApp/login.htm");
+}
+@Then("User enters  name {string}")
+public void user_enters_name(String unamee) {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .xpath("//*[@id=\"userName\"]")).sendKeys(unamee);
+}
 
+@Then("User enters  pas {string}")
+public void user_enters_pas(String passwrd) {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .xpath("//*[@id=\"password\"]")).sendKeys(passwrd);
+}
 
+@Then("user clicks on login button")
+public void user_clicks_on_login_button(){
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
 
-	private WebDriver driver;
-	static int numberOfProducts; 
-	@Given("Alex is on the login page")
-	public void alex_is_on_the_login_page() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_h2a.06.15\\Desktop\\SeleniumBroDrivers\\SeleniumBroDrivers\\chromedriver.exe\\");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.get("http://10.232.237.143:443/TestMeApp/login.htm");
-		
-	}
+	driver.findElement(By .name("Login")).click();
+    
+}
 
-	@When("Alex enters correct credentials")
-	public void alex_enters_correct_credentials() {
-		driver.findElement(By.name("userName")).sendKeys("Lalitha");
-		driver.findElement(By.name("password")).sendKeys("Password123");
-		driver.findElement(By.name("Login")).click();
-	}
+@Given("Product Search Engine")
+public void product_Search_Engine() {
+	 Actions a=new Actions(driver);
+	 a.moveToElement(driver.findElement(By .xpath("//*[@id=\"menu3\"]/li[2]/a/span"))).click().perform();
+	 a.moveToElement(driver.findElement(By .xpath("//*[@id=\"menu3\"]/li[2]/ul/li[1]/a/span"))).click().perform();
+	 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	 a.moveToElement(driver.findElement(By .xpath("//*[@id=\"submenuul11290\"]/li[1]/a/span"))).click().perform();
+	
+}
 
+@Then("User proceed towards adding product to cart")
+public void user_proceed_towards_adding_product_to_cart() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+    driver.findElement(By .xpath("/html/body/section/div/div/div[2]/div/div/div/div[2]/center/a")).click();
+}
 
+@Then("User click on cart link")
+public void user_click_on_cart_link() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+   driver.findElement(By .xpath("//*[@id=\"header\"]/div[1]/div/div/div[2]/div/a[2]")).click();
+}
 
-	@When("Alex enters username {string}")
-	public void alex_enters_username(String username) {
-		driver.findElement(By.name("userName")).sendKeys(username);
+@Then("User verifies the product is correctly added to the cart")
+public void user_verifies_the_product_is_correctly_added_to_the_cart() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	boolean b= driver.findElement(By .xpath("//*[@id=\"cart\"]/tfoot/tr[2]/td[5]/a")).isDisplayed();
+	Assert.assertTrue(b);
+}
 
-	}
+@Then("user clicks on checkout for payment")
+public void user_clicks_on_checkout_for_payment() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .xpath("//*[@id=\"cart\"]/tfoot/tr[2]/td[5]/a")).click();
+}
 
-	@When("Alex enters password {string}")
-	public void alex_enters_password(String password) {
-		driver.findElement(By.name("password")).sendKeys(password);
-	}
+@Then("User Review the product and click on proceed to pay")
+public void user_Review_the_product_and_click_on_proceed_to_pay() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .xpath("/html/body/b/div/div/div[1]/div/div[2]/div[3]/div/form[2]/input")).click();
+}
 
-	@When("Alex click on login")
-	public void alex_click_on_login() {
-		driver.findElement(By.name("Login")).click();
-	}
+@Then("User select the bank of his\\/her choice")
+public void user_select_the_bank_of_his_her_choice() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .xpath("//*[@id=\"swit\"]/div[1]/div/label")).click();
+}
 
-	@Then("Alex can do successful login")
-	public void alex_can_do_successful_login() {
-		Assert.assertTrue(driver.getTitle().contains("Home"));
+@Then("User clicks on continue for further details")
+public void user_clicks_on_continue_for_further_details() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .xpath("//*[@id=\"btn\"]")).click();
+}
 
-	}
+@Then("User give valid credentials according to selected bank")
+public void user_give_valid_credentials_according_to_selected_bank() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	driver.findElement(By .name("username")).sendKeys("123456");
+	driver.findElement(By .name("password")).sendKeys("Pass@456");
+	driver.findElement(By .xpath("//*[@id=\"horizontalTab\"]/div[2]/div/div/div/div/form/div/div[3]/input")).click();
+}
 
+@Then("User than give the required transaction password")
+public void user_than_give_the_required_transaction_password() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	 driver.findElement(By .name("transpwd")).sendKeys("Trans@456");
+	 driver.findElement(By .xpath("//*[@id=\"horizontalTab\"]/div[2]/div/div/div/div/form/div/div[2]/input")).click();
+}
 
-	@When("user searches the below product:")
-	public void user_searches_the_below_product(DataTable productsName) {
-		driver.findElement(By.name("userName")).sendKeys("Lalitha");
-		driver.findElement(By.name("password")).sendKeys("Password123");
-		driver.findElement(By.name("Login")).click();
-		List<String> products = productsName.asList();
+@Then("User verifies for the order is confirmed")
+public void user_verifies_for_the_order_is_confirmed() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	String expected_confirmation="Your order has been confirmed";
+	String actual_confirmation=driver.findElement(By .xpath("/html/body/b/section/div/div/div/div[2]/p")).getText();
+	Assert.assertEquals(expected_confirmation, actual_confirmation);
+}
 
-		for(String eachProduct : products){
-			driver.findElement(By.id("myInput")).sendKeys(eachProduct);
-			driver.findElement(By.xpath("/html/body/div[1]/form/input")).click();
+@Given("User registered in TestMeApp")
+public void user_registered_in_TestMeApp() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_h2a.06.15\\Desktop\\SeleniumBroDrivers\\SeleniumBroDrivers\\chromedriver.exe\\");
+	driver =new ChromeDriver();
+	driver.get("http://10.232.237.143:443/TestMeApp/login.htm");
+	driver.findElement(By .name("userName")).sendKeys("lalitha");
+	driver.findElement(By .name("password")).sendKeys("Password123");
+	driver.findElement(By .name("Login")).click();
+}
 
-			try{
-				driver.findElement(By.linkText("Add to cart")).click();
-				numberOfProducts++;	
-			}catch(NoSuchElementException|StaleElementReferenceException  e){
+@When("User Search for product like headphones")
+public void user_Search_for_product_like_headphones() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	 Actions a=new Actions(driver);
+	 driver.findElement(By .name("products")).sendKeys("head");
+	 a.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+}
 
-			}
-			driver.findElement(By.linkText("Home")).click();
-		}
-	}
-	@Then("available products should be added to cart")
-	public void available_products_should_be_added_to_cart() {
+@When("try to proceed to payment without adding any item in the cart")
+public void try_to_proceed_to_payment_without_adding_any_item_in_the_cart() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	boolean cart=driver.findElement(By .xpath("//*[@id=\"header\"]/div[1]/div/div/div[2]/div/a[2]")).isDisplayed();
+	Assert.assertTrue(cart);
+}
 
-		WebElement  countofProducts =driver.findElement(By.xpath("//*[@id='header']/div[1]/div/div/div[2]/div/a[2]/b"));
-		String countProducts = countofProducts.getText();
-		int  countProductsInInt = Integer.parseInt(countProducts);
-		Assert.assertEquals(numberOfProducts, countProductsInInt);
-	}
-
-
-
-
-
-
-
+@Then("TestMe doesn't display the cart icon")
+public void testme_doesn_t_display_the_cart_icon() {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new cucumber.api.PendingException();
+	System.out.println("No cart icon displayed");
+	
+}
 }
